d7827bde6d082369140910549651dfb414038ae1
gr-ishiguro <mizuki.ishiguro@gmo-research.jp>
2016-11-23 02:36:47 +0900

    fix config

--- config/app.php
+++ config/app.php
@@ -215,7 +215,7 @@ return [
             'className' => 'Cake\Database\Connection',
             'driver' => 'Cake\Database\Driver\Postgres',
             'persistent' => false,
-            'unix_socket' => '/cloudsql/develop',
+            'unix_socket' => '',
             /**
              * CakePHP will use the default DB port based on the driver selected
              * MySQL on MAMP uses port 8889, MAMP users will want to uncomment
