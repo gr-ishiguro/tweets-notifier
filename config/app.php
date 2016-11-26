b6f9c35e3eabd276748ecb11b28b9bc7ee3015f8
gr-ishiguro <mizuki.ishiguro@gmo-research.jp>
2016-11-26 13:00:26 +0900

    refactoring

--- config/app.php
+++ config/app.php
@@ -86,9 +86,6 @@ return [
     'Cache' => [
         'default' => [
             'className' => 'Memcached',
-            'servers'   => env('MEMCACHIER_SERVERS'),
-            'username'  => env('MEMCACHIER_USERNAME'),
-            'password'  => env('MEMCACHIER_PASSWORD'),
         ],
 
         /**
@@ -101,6 +98,9 @@ return [
             'className' => 'Memcached',
             'prefix' => 'myapp_cake_core_',
             'duration' => '+2 minutes',
+            'servers'   => env('MEMCACHIER_SERVERS'),
+            'username'  => env('MEMCACHIER_USERNAME'),
+            'password'  => env('MEMCACHIER_PASSWORD'),
         ],
 
         /**
